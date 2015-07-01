8055b3f59b7b7254300b9342c1a4fef6b64d4cf1
kuroro <kuroro@apple4deiMac.local>
2015-07-01 09:49:57 +0800

    add new testGit class

---
+++ Demo01-Git-1/TestGit.h
@@ -0,0 +1,13 @@
+//
+//  TestGit.h
+//  Demo01-Git-1
+//
+//  Created by kuroro on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestGit : NSObject
+
+@end
---
+++ Demo01-Git-1/TestGit.m
@@ -0,0 +1,19 @@
+//
+//  TestGit.m
+//  Demo01-Git-1
+//
+//  Created by kuroro on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "TestGit.h"
+
+@implementation TestGit
+
+-(void)testFunction{
+
+    NSLog(@"123321");
+
+}
+
+@end
